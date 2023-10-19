d7d0581e0e92d45658ccf0f5c0edd9b8a861a0be
Ricardo Boluda <ricardo.boluda@cabify.com>
2023-10-19 11:31:58 +0200

    Update CSS

--- scripts/popupScript.js
+++ scripts/popupScript.js
@@ -1,24 +1,27 @@
+console.log("Script loaded");
 
-console.log('Script loaded')
+document.addEventListener("DOMContentLoaded", async function () {
+  const tab = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  const switchId = "switch";
+  const demoSwitch = document.getElementById(switchId);
 
-document.addEventListener('DOMContentLoaded', async function () {
-
-  const tab = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-  const switchId = 'switch'
-  const demoSwitch = document.getElementById(switchId)
-
-  console.log('TAB ' + JSON.stringify(tab));
+  console.log("TAB " + JSON.stringify(tab));
 
   await chrome.scripting.executeScript({
     target: { tabId: tab[0].id },
     func: async () => {
-      const currentStatus = localStorage.getItem('demo')
-      await chrome.storage.local.set({'demo': currentStatus})
+      const currentStatus = localStorage.getItem("demo");
+      await chrome.storage.local.set({
+        demo: currentStatus === "true" ? true : false,
+      });
     },
   });
 
   chrome.storage.local.get("demo").then((result) => {
-    console.log('Get ' + JSON.stringify(result));
+    console.log("Get " + JSON.stringify(result));
     demoSwitch.checked = result.demo;
   });
 
@@ -28,22 +31,21 @@ document.addEventListener('DOMContentLoaded', async function () {
     console.log('WRITE ' + newStatus);
 
     if (newStatus) {
-      activateDemoMode(tab[0])
-      hideSudoWarning(tab[0])
-    }
-    else {
-      deactivateDemoMode(tab[0])
-      showSudoWarning(tab[0])
+      activateDemoMode(tab[0]);
+      hideSudoWarning(tab[0]);
+    } else {
+      deactivateDemoMode(tab[0]);
+      showSudoWarning(tab[0]);
     }
   });
-
 });
 
 async function activateDemoMode(tab) {
   await chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: () => {
-      localStorage.setItem('demo', true);
+      localStorage.setItem("demo", true);
+      window.location.reload();
     },
   });
 }
@@ -52,25 +54,33 @@ async function deactivateDemoMode(tab) {
   await chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: () => {
-      localStorage.setItem('demo', false);
+      localStorage.setItem("demo", false);
+      window.location.reload();
     },
   });
 }
 
 // Hide the impersonated card on the sidebar.
 async function hideSudoWarning(tab) {
-    // Insert the CSS file when the user turns the extension on
-    await chrome.scripting.insertCSS({
-        files: ['focus-mode.css'],
-        target: { tabId: tab.id }
-    });
+  // Insert the CSS file when the user turns the extension on
+  await chrome.scripting.insertCSS({
+    files: ["focus-mode.css"],
+    target: { tabId: tab.id },
+  });
 }
 
 // Show the impersonated card on the sidebar.
 async function showSudoWarning(tab) {
-    // Remove the CSS file when the user turns the extension off
-    await chrome.scripting.removeCSS({
-        files: ['focus-mode.css'],
-        target: { tabId: tab.id }
-    });
+  // Remove the CSS file when the user turns the extension off
+  await chrome.scripting.removeCSS({
+    files: ["focus-mode.css"],
+    target: { tabId: tab.id },
+  });
+}
+
+async function updateBadge(tab, text) {
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: text,
+  });
 }
